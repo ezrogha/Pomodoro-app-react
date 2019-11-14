9f1bdd17533ccd6e54553dceccd29c85fa522cf2
Roghashin Timbiti <rtimbiti@gmail.com>
2019-11-14 19:34:24 +0300

    Updates

---
+++ precache-manifest.70acfe723b3b34b31a41b18f91849ec8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee799a3d7e45ab76d1267a73738ee66",
+    "url": "/Pomodoro-app-react/index.html"
+  },
+  {
+    "revision": "7c60408c26efab95b475",
+    "url": "/Pomodoro-app-react/static/css/2.b713a164.chunk.css"
+  },
+  {
+    "revision": "7e5cc1bcb2a55832202e",
+    "url": "/Pomodoro-app-react/static/css/main.50ae6cda.chunk.css"
+  },
+  {
+    "revision": "7c60408c26efab95b475",
+    "url": "/Pomodoro-app-react/static/js/2.0bc2dcf8.chunk.js"
+  },
+  {
+    "revision": "7e5cc1bcb2a55832202e",
+    "url": "/Pomodoro-app-react/static/js/main.23974a23.chunk.js"
+  },
+  {
+    "revision": "025ee1a70d7675208fb9",
+    "url": "/Pomodoro-app-react/static/js/runtime-main.b0877ffe.js"
+  },
+  {
+    "revision": "e9acea510652fd2dd866a8bf817e13bf",
+    "url": "/Pomodoro-app-react/static/media/aron-visuals-BXOXnQ26B7o-unsplash.e9acea51.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Pomodoro-app-react/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Pomodoro-app-react/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Pomodoro-app-react/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Pomodoro-app-react/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Pomodoro-app-react/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "6e4413c48e8d83537b178670961fedeb",
+    "url": "/Pomodoro-app-react/static/media/bus-horn.6e4413c4.mp3"
+  },
+  {
+    "revision": "33d46e7cf3159d286d3129c9b5feab74",
+    "url": "/Pomodoro-app-react/static/media/favicon.33d46e7c.ico"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Pomodoro-app-react/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Pomodoro-app-react/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Pomodoro-app-react/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Pomodoro-app-react/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Pomodoro-app-react/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Pomodoro-app-react/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Pomodoro-app-react/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Pomodoro-app-react/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Pomodoro-app-react/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Pomodoro-app-react/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Pomodoro-app-react/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
